8c4c726e986e5204a791303657c90649a962b57b
SanghyukChun <sanghyuk.chun@gmail.com>
2013-07-24 03:05:18 +0900

    add cancle btn click event

--- assets/js/aboutMe.js
+++ assets/js/aboutMe.js
@@ -14,3 +14,8 @@ $(document).ready(function() {
 		});
 	}
 });
+
+$("#contact_cancle_btn").click(function() {
+	$("#contact-form>.control-group>.controls>input").val('')
+	$("#contact-form>.control-group>.controls>textarea").val('')
+})
