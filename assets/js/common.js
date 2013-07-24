6feb2a97736fe9212e7e496ec87c198ecd95d218
SanghyukChun <sanghyuk.chun@gmail.com>
2013-07-24 09:07:22 +0900

    add semicolon

--- assets/js/common.js
+++ assets/js/common.js
@@ -15,4 +15,4 @@ $(document).ready(function() {
 $("#contact_cancle_btn").click(function() {
 	$("#contact-form>.control-group>.controls>input").val('')
 	$("#contact-form>.control-group>.controls>textarea").val('')
-})
+});
