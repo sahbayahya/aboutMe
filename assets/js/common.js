f71c39af3cf25058efc788e42bb4b5430f057594
SanghyukChun <sanghyuk.chun@gmail.com>
2013-07-24 09:02:15 +0900

    edit ajax to get

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
 	if($('#github_work').length > 0) {
 		$.support.cors = true;
-		$.ajax({
-			url: 'https://api.github.com/users/SanghyukChun/repos', 
-			dataType: 'json', 
-			success: function(result) {
-				$.each(result, function(idx, repo) {
-					var name = repo.name
-					var url = repo.html_url
-					var desc = repo.description
-					$('#github_work').append("<li><a href="+url+">"+name+"</a>: "+desc+"</li>")
-				});
-			}
+		$.get('https://api.github.com/users/SanghyukChun/repos', function(result) {
+			$.each(result, function(idx, repo) {
+				var name = repo.name
+				var url = repo.html_url
+				var desc = repo.description
+				$('#github_work').append("<li><a href="+url+">"+name+"</a>: "+desc+"</li>")
+			});
 		});
 	}
 });
